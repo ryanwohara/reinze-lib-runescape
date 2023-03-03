6c32f43d36a8ec148fb5902aa9bed54b92b60a12
Ryan William O'Hara <ryan@ryanwohara.com>
2023-03-02 19:27:23 -0700

    revert: Stats
    We need to get the fix for the bosses command
    deployed, so we will revert the stats commands
    and work on that in a branch instead.

--- src/lib.rs
+++ src/lib.rs
@@ -21,21 +21,21 @@ pub extern "C" fn exported(command: &str, query: &str, author: &str) -> Result<V
         "patch" => patch::patch(query),
         "players" => players::players(),
         "price" => prices::prices(query),
-        "overall" | "stats" | "total" | "attack" | "att" | "defence" | "def" | "strength"
-        | "str" | "hitpoints" | "hp" | "ranged" | "range" | "prayer" | "pray" | "magic"
-        | "mage" | "cooking" | "cook" | "woodcutting" | "wc" | "fletching" | "fletch"
-        | "fishing" | "fish" | "firemaking" | "fm" | "crafting" | "craft" | "smithing"
-        | "smith" | "mining" | "mine" | "herblore" | "herb" | "agility" | "agil" | "thieving"
-        | "thief" | "slayer" | "slay" | "farming" | "farm" | "runecraft" | "rc" | "hunter"
-        | "hunt" | "construction" | "con" => stats::stats(command, query, author),
+        // "overall" | "stats" | "total" | "attack" | "att" | "defence" | "def" | "strength"
+        // | "str" | "hitpoints" | "hp" | "ranged" | "range" | "prayer" | "pray" | "magic"
+        // | "mage" | "cooking" | "cook" | "woodcutting" | "wc" | "fletching" | "fletch"
+        // | "fishing" | "fish" | "firemaking" | "fm" | "crafting" | "craft" | "smithing"
+        // | "smith" | "mining" | "mine" | "herblore" | "herb" | "agility" | "agil" | "thieving"
+        // | "thief" | "slayer" | "slay" | "farming" | "farm" | "runecraft" | "rc" | "hunter"
+        // | "hunt" | "construction" | "con" => stats::stats(command, query, author),
         "help" => Ok("boss
 ge
 level
 xp
 params
 players
-price
-stats"
+price"
+            //stats"
             .split("\n")
             .map(|s| s.to_string())
             .collect::<Vec<String>>()),
@@ -49,59 +49,59 @@ players
 price
 xp
 exp
-experience
-stats
-overall
-total
-attack
-att
-defence
-def
-strength
-str
-hitpoints
-hp
-ranged
-range
-prayer
-pray
-magic
-mage
-cooking
-cook
-woodcutting
-wc
-fletching
-fletch
-fishing
-fish
-firemaking
-fm
-crafting
-craft
-smithing
-smith
-mining
-mine
-herblore
-herb
-agility
-agil
-thieving
-thief
-slayer
-slay
-farming
-farm
-runecraft
-rc
-hunter
-hunt
-construction
-con"
-        .split("\n")
-        .map(|s| s.to_string())
-        .collect::<Vec<String>>()),
+experience"
+            // stats
+            // overall
+            // total
+            // attack
+            // att
+            // defence
+            // def
+            // strength
+            // str
+            // hitpoints
+            // hp
+            // ranged
+            // range
+            // prayer
+            // pray
+            // magic
+            // mage
+            // cooking
+            // cook
+            // woodcutting
+            // wc
+            // fletching
+            // fletch
+            // fishing
+            // fish
+            // firemaking
+            // fm
+            // crafting
+            // craft
+            // smithing
+            // smith
+            // mining
+            // mine
+            // herblore
+            // herb
+            // agility
+            // agil
+            // thieving
+            // thief
+            // slayer
+            // slay
+            // farming
+            // farm
+            // runecraft
+            // rc
+            // hunter
+            // hunt
+            // construction
+            // con"
+            .split("\n")
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()),
         _ => Ok(vec![]),
     }
 }
