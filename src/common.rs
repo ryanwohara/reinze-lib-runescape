4bbcd3f891e678bd0b9b2f4cf3150a9ef0eaeaf7
Ryan William O'Hara <ryan@ryanwohara.com>
2023-11-05 16:22:14 -0700

    ci: Adding initial tests

--- src/common.rs
+++ src/common.rs
@@ -228,3 +228,343 @@ fn query_stats(rsn: &str) -> core::result::Result<String, ()> {
 
     Ok(body)
 }
+
+#[cfg(test)]
+mod tests {
+    // import names from outer (for mod tests) scope
+    use super::*;
+
+    #[test]
+    fn test_capitalize() {
+        assert_eq!(capitalize("hello"), "Hello");
+        assert_eq!(capitalize("Hello"), "Hello");
+        assert_eq!(capitalize("HELLO"), "Hello");
+        assert_eq!(capitalize("hELLO"), "Hello");
+        assert_eq!(capitalize("hElLo"), "Hello");
+        assert_eq!(capitalize("123"), "123");
+        assert_eq!(capitalize(""), "");
+    }
+
+    #[test]
+    fn test_c1() {
+        assert_eq!(c1("hello"), "\x0314hello");
+        assert_eq!(c1("Hello"), "\x0314Hello");
+        assert_eq!(c1("HELLO"), "\x0314HELLO");
+        assert_eq!(c1("hELLO"), "\x0314hELLO");
+        assert_eq!(c1("hElLo"), "\x0314hElLo");
+        assert_eq!(c1("123"), "\x0314123");
+        assert_eq!(c1(""), "\x0314");
+    }
+
+    #[test]
+    fn test_c2() {
+        assert_eq!(c2("hello"), "\x0304hello");
+        assert_eq!(c2("Hello"), "\x0304Hello");
+        assert_eq!(c2("HELLO"), "\x0304HELLO");
+        assert_eq!(c2("hELLO"), "\x0304hELLO");
+        assert_eq!(c2("hElLo"), "\x0304hElLo");
+        assert_eq!(c2("123"), "\x0304123");
+        assert_eq!(c2(""), "\x0304");
+    }
+
+    #[test]
+    fn test_c3() {
+        assert_eq!(c3("hello"), "\x0305hello");
+        assert_eq!(c3("Hello"), "\x0305Hello");
+        assert_eq!(c3("HELLO"), "\x0305HELLO");
+        assert_eq!(c3("hELLO"), "\x0305hELLO");
+        assert_eq!(c3("hElLo"), "\x0305hElLo");
+        assert_eq!(c3("123"), "\x0305123");
+        assert_eq!(c3(""), "\x0305");
+    }
+
+    #[test]
+    fn test_c4() {
+        assert_eq!(c4("hello"), "\x0303hello");
+        assert_eq!(c4("Hello"), "\x0303Hello");
+        assert_eq!(c4("HELLO"), "\x0303HELLO");
+        assert_eq!(c4("hELLO"), "\x0303hELLO");
+        assert_eq!(c4("hElLo"), "\x0303hElLo");
+        assert_eq!(c4("123"), "\x0303123");
+        assert_eq!(c4(""), "\x0303");
+    }
+
+    #[test]
+    fn test_c5() {
+        assert_eq!(c5("hello"), "\x0307hello");
+        assert_eq!(c5("Hello"), "\x0307Hello");
+        assert_eq!(c5("HELLO"), "\x0307HELLO");
+        assert_eq!(c5("hELLO"), "\x0307hELLO");
+        assert_eq!(c5("hElLo"), "\x0307hElLo");
+        assert_eq!(c5("123"), "\x0307123");
+        assert_eq!(c5(""), "\x0307");
+    }
+
+    #[test]
+    fn test_l() {
+        assert_eq!(l("hello"), "\x0314[\x0304hello\x0314]");
+        assert_eq!(l("Hello"), "\x0314[\x0304Hello\x0314]");
+        assert_eq!(l("HELLO"), "\x0314[\x0304HELLO\x0314]");
+        assert_eq!(l("hELLO"), "\x0314[\x0304hELLO\x0314]");
+        assert_eq!(l("hElLo"), "\x0314[\x0304hElLo\x0314]");
+        assert_eq!(l("123"), "\x0314[\x0304123\x0314]");
+        assert_eq!(l(""), "\x0314[\x0304\x0314]");
+    }
+
+    #[test]
+    fn test_p() {
+        assert_eq!(p("hello"), "\x0314(\x0304hello\x0314)");
+        assert_eq!(p("Hello"), "\x0314(\x0304Hello\x0314)");
+        assert_eq!(p("HELLO"), "\x0314(\x0304HELLO\x0314)");
+        assert_eq!(p("hELLO"), "\x0314(\x0304hELLO\x0314)");
+        assert_eq!(p("hElLo"), "\x0314(\x0304hElLo\x0314)");
+        assert_eq!(p("123"), "\x0314(\x0304123\x0314)");
+        assert_eq!(p(""), "\x0314(\x0304\x0314)");
+    }
+
+    #[test]
+    fn test_level_to_xp() {
+        assert_eq!(level_to_xp(1), 0);
+        assert_eq!(level_to_xp(2), 83);
+        assert_eq!(level_to_xp(3), 174);
+        assert_eq!(level_to_xp(4), 275);
+        assert_eq!(level_to_xp(5), 387);
+        assert_eq!(level_to_xp(6), 511);
+        assert_eq!(level_to_xp(7), 648);
+        assert_eq!(level_to_xp(8), 799);
+        assert_eq!(level_to_xp(9), 966);
+        assert_eq!(level_to_xp(10), 1151);
+        assert_eq!(level_to_xp(11), 1355);
+        assert_eq!(level_to_xp(12), 1580);
+        assert_eq!(level_to_xp(13), 1829);
+        assert_eq!(level_to_xp(14), 2103);
+        assert_eq!(level_to_xp(15), 2406);
+        assert_eq!(level_to_xp(16), 2740);
+        assert_eq!(level_to_xp(17), 3109);
+        assert_eq!(level_to_xp(18), 3517);
+        assert_eq!(level_to_xp(19), 3967);
+        assert_eq!(level_to_xp(20), 4463);
+        assert_eq!(level_to_xp(21), 5011);
+        assert_eq!(level_to_xp(22), 5616);
+        assert_eq!(level_to_xp(23), 6283);
+        assert_eq!(level_to_xp(24), 7020);
+        assert_eq!(level_to_xp(25), 7833);
+        assert_eq!(level_to_xp(26), 8730);
+        assert_eq!(level_to_xp(27), 9720);
+        assert_eq!(level_to_xp(28), 10813);
+        assert_eq!(level_to_xp(29), 12020);
+        assert_eq!(level_to_xp(30), 13352);
+        assert_eq!(level_to_xp(45), 61495);
+        assert_eq!(level_to_xp(55), 166614);
+        assert_eq!(level_to_xp(75), 1210391);
+        assert_eq!(level_to_xp(92), 6517217);
+        assert_eq!(level_to_xp(95), 8771521);
+        assert_eq!(level_to_xp(96), 9684539);
+        assert_eq!(level_to_xp(97), 10692591);
+        assert_eq!(level_to_xp(98), 11805568);
+        assert_eq!(level_to_xp(99), 13034392);
+    }
+    #[test]
+    fn test_xp_to_level() {
+        assert_eq!(xp_to_level(0), 1);
+        assert_eq!(xp_to_level(83), 2);
+        assert_eq!(xp_to_level(174), 3);
+        assert_eq!(xp_to_level(275), 4);
+        assert_eq!(xp_to_level(387), 5);
+        assert_eq!(xp_to_level(511), 6);
+        assert_eq!(xp_to_level(648), 7);
+        assert_eq!(xp_to_level(799), 8);
+        assert_eq!(xp_to_level(966), 9);
+        assert_eq!(xp_to_level(1151), 10);
+        assert_eq!(xp_to_level(1355), 11);
+        assert_eq!(xp_to_level(1580), 12);
+        assert_eq!(xp_to_level(1829), 13);
+        assert_eq!(xp_to_level(2103), 14);
+        assert_eq!(xp_to_level(2406), 15);
+        assert_eq!(xp_to_level(2740), 16);
+        assert_eq!(xp_to_level(3109), 17);
+        assert_eq!(xp_to_level(3517), 18);
+        assert_eq!(xp_to_level(3967), 19);
+        assert_eq!(xp_to_level(4463), 20);
+        assert_eq!(xp_to_level(5011), 21);
+        assert_eq!(xp_to_level(5616), 22);
+        assert_eq!(xp_to_level(6283), 23);
+        assert_eq!(xp_to_level(7020), 24);
+        assert_eq!(xp_to_level(7833), 25);
+        assert_eq!(xp_to_level(8730), 26);
+        assert_eq!(xp_to_level(9720), 27);
+        assert_eq!(xp_to_level(10813), 28);
+        assert_eq!(xp_to_level(12020), 29);
+        assert_eq!(xp_to_level(13352), 30);
+        assert_eq!(xp_to_level(61495), 45);
+        assert_eq!(xp_to_level(166614), 55);
+        assert_eq!(xp_to_level(1210391), 75);
+        assert_eq!(xp_to_level(6517217), 92);
+        assert_eq!(xp_to_level(8771521), 95);
+        assert_eq!(xp_to_level(9684539), 96);
+        assert_eq!(xp_to_level(10692591), 97);
+        assert_eq!(xp_to_level(11805568), 98);
+        assert_eq!(xp_to_level(13034392), 99);
+    }
+
+    #[test]
+    fn test_commas() {
+        assert_eq!(commas(0.0), "0");
+        assert_eq!(commas(1.0), "1");
+        assert_eq!(commas(10.0), "10");
+        assert_eq!(commas(100.0), "100");
+        assert_eq!(commas(1000.0), "1,000");
+        assert_eq!(commas(10000.0), "10,000");
+        assert_eq!(commas(100000.0), "100,000");
+        assert_eq!(commas(1000000.0), "1,000,000");
+        assert_eq!(commas(10000000.0), "10,000,000");
+        assert_eq!(commas(100000000.0), "100,000,000");
+        assert_eq!(commas(1000000000.0), "1,000,000,000");
+        assert_eq!(commas(10000000000.0), "10,000,000,000");
+        assert_eq!(commas(100000000000.0), "100,000,000,000");
+        assert_eq!(commas(1000000000000.0), "1,000,000,000,000");
+        assert_eq!(commas(10000000000000.0), "10,000,000,000,000");
+        assert_eq!(commas(100000000000000.0), "100,000,000,000,000");
+        assert_eq!(commas(1000000000000000.0), "1,000,000,000,000,000");
+        assert_eq!(commas(10000000000000000.0), "10,000,000,000,000,000");
+        assert_eq!(commas(100000000000000000.0), "100,000,000,000,000,000");
+        assert_eq!(commas(1000000000000000000.0), "1,000,000,000,000,000,000");
+        assert_eq!(commas(10000000000000000000.0), "10,000,000,000,000,000,000");
+        assert_eq!(
+            commas(100000000000000000000.0),
+            "100,000,000,000,000,000,000"
+        );
+    }
+
+    #[test]
+    fn test_commas_from_string() {
+        assert_eq!(commas_from_string("0"), "0");
+        assert_eq!(commas_from_string("1"), "1");
+        assert_eq!(commas_from_string("10"), "10");
+        assert_eq!(commas_from_string("100"), "100");
+        assert_eq!(commas_from_string("1000"), "1,000");
+        assert_eq!(commas_from_string("10000"), "10,000");
+        assert_eq!(commas_from_string("100000"), "100,000");
+        assert_eq!(commas_from_string("1000000"), "1,000,000");
+        assert_eq!(commas_from_string("10000000"), "10,000,000");
+        assert_eq!(commas_from_string("100000000"), "100,000,000");
+        assert_eq!(commas_from_string("1000000000"), "1,000,000,000");
+        assert_eq!(commas_from_string("10000000000"), "10,000,000,000");
+        assert_eq!(commas_from_string("100000000000"), "100,000,000,000");
+        assert_eq!(commas_from_string("1000000000000"), "1,000,000,000,000");
+        assert_eq!(commas_from_string("10000000000000"), "10,000,000,000,000");
+        assert_eq!(commas_from_string("100000000000000"), "100,000,000,000,000");
+        assert_eq!(
+            commas_from_string("1000000000000000"),
+            "1,000,000,000,000,000"
+        );
+        assert_eq!(
+            commas_from_string("10000000000000000"),
+            "10,000,000,000,000,000"
+        );
+        assert_eq!(
+            commas_from_string("100000000000000000"),
+            "100,000,000,000,000,000"
+        );
+        assert_eq!(
+            commas_from_string("1000000000000000000"),
+            "1,000,000,000,000,000,000"
+        );
+        assert_eq!(
+            commas_from_string("10000000000000000000"),
+            "10,000,000,000,000,000,000"
+        );
+        assert_eq!(
+            commas_from_string("100000000000000000000"),
+            "100,000,000,000,000,000,000"
+        );
+    }
+
+    #[test]
+    fn test_skill() {
+        assert_eq!(skill("overall"), "Overall");
+        assert_eq!(skill("stats"), "Overall");
+        assert_eq!(skill("total"), "Overall");
+        assert_eq!(skill("attack"), "Attack");
+        assert_eq!(skill("att"), "Attack");
+        assert_eq!(skill("defence"), "Defence");
+        assert_eq!(skill("def"), "Defence");
+        assert_eq!(skill("strength"), "Strength");
+        assert_eq!(skill("str"), "Strength");
+        assert_eq!(skill("hitpoints"), "Hitpoints");
+        assert_eq!(skill("hp"), "Hitpoints");
+        assert_eq!(skill("ranged"), "Ranged");
+        assert_eq!(skill("range"), "Ranged");
+        assert_eq!(skill("prayer"), "Prayer");
+        assert_eq!(skill("pray"), "Prayer");
+        assert_eq!(skill("magic"), "Magic");
+        assert_eq!(skill("mage"), "Magic");
+        assert_eq!(skill("cooking"), "Cooking");
+        assert_eq!(skill("cook"), "Cooking");
+        assert_eq!(skill("woodcutting"), "Woodcutting");
+        assert_eq!(skill("wc"), "Woodcutting");
+        assert_eq!(skill("fletching"), "Fletching");
+        assert_eq!(skill("fletch"), "Fletching");
+        assert_eq!(skill("fishing"), "Fishing");
+        assert_eq!(skill("fish"), "Fishing");
+        assert_eq!(skill("firemaking"), "Firemaking");
+        assert_eq!(skill("fm"), "Firemaking");
+        assert_eq!(skill("crafting"), "Crafting");
+        assert_eq!(skill("craft"), "Crafting");
+        assert_eq!(skill("smithing"), "Smithing");
+        assert_eq!(skill("smith"), "Smithing");
+        assert_eq!(skill("mining"), "Mining");
+        assert_eq!(skill("mine"), "Mining");
+        assert_eq!(skill("herblore"), "Herblore");
+        assert_eq!(skill("herb"), "Herblore");
+        assert_eq!(skill("agility"), "Agility");
+        assert_eq!(skill("agil"), "Agility");
+        assert_eq!(skill("thieving"), "Thieving");
+        assert_eq!(skill("thief"), "Thieving");
+        assert_eq!(skill("slayer"), "Slayer");
+        assert_eq!(skill("slay"), "Slayer");
+        assert_eq!(skill("farming"), "Farming");
+        assert_eq!(skill("farm"), "Farming");
+        assert_eq!(skill("runecraft"), "Runecraft");
+        assert_eq!(skill("rc"), "Runecraft");
+        assert_eq!(skill("hunter"), "Hunter");
+        assert_eq!(skill("hunt"), "Hunter");
+        assert_eq!(skill("construction"), "Construction");
+        assert_eq!(skill("con"), "Construction");
+        assert_eq!(skill("invalid"), "");
+    }
+
+    #[test]
+    fn test_skills() {
+        assert_eq!(skills().len(), 24,);
+        assert_eq!(
+            skills(),
+            vec![
+                "Overall".to_string(),
+                "Attack".to_string(),
+                "Defence".to_string(),
+                "Strength".to_string(),
+                "Hitpoints".to_string(),
+                "Ranged".to_string(),
+                "Prayer".to_string(),
+                "Magic".to_string(),
+                "Cooking".to_string(),
+                "Woodcutting".to_string(),
+                "Fletching".to_string(),
+                "Fishing".to_string(),
+                "Firemaking".to_string(),
+                "Crafting".to_string(),
+                "Smithing".to_string(),
+                "Mining".to_string(),
+                "Herblore".to_string(),
+                "Agility".to_string(),
+                "Thieving".to_string(),
+                "Slayer".to_string(),
+                "Farming".to_string(),
+                "Runecraft".to_string(),
+                "Hunter".to_string(),
+                "Construction".to_string(),
+            ]
+        );
+    }
+}
