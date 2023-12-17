c6f8680500bd789fea497c2965b035413d94c4a6
Ryan William O'Hara <ryan@ryanwohara.com>
2023-12-16 21:12:27 -0700

    feat: -noburn [filter]

--- src/noburn.rs
+++ src/noburn.rs
@@ -1,7 +1,7 @@
 use common::{c1, c2, l, p};
 
 pub fn noburn(query: &str) -> Result<Vec<String>, ()> {
-    let fish = vec![
+    let fish: Vec<Fish> = vec![
         Fish::new("Tuna", 65, 0),
         Fish::new("Lobster", 74, 68),
         Fish::new("Swordfish", 86, 81),
@@ -9,7 +9,11 @@ pub fn noburn(query: &str) -> Result<Vec<String>, ()> {
         Fish::new("Shark", 100, 94),
         Fish::new("Anglerfish", 0, 98),
     ];
-    let output: Vec<String> = fish.into_iter().map(|fish| fish.to_string()).collect();
+    let output: Vec<String> = fish
+        .into_iter()
+        .filter(|fish| fish_finder(fish, query))
+        .map(|fish| fish.to_string())
+        .collect();
     return Ok(vec![format!("{} {}", l("NoBurn"), output.join(&c1(" | ")))]);
 }
 
@@ -31,8 +35,21 @@ impl Fish {
         format!(
             "{} {} {}",
             c1(self.name),
-            c2(&self.normal.to_string()),
-            p(&self.gauntlet.to_string())
+            c2(&if_not_available(self.normal)),
+            p(&if_not_available(self.gauntlet))
         )
     }
+}
+
+fn if_not_available(int: u32) -> String {
+    if int == 0 {
+        return c2("N/A");
+    }
+
+    int.to_string()
+}
+
+fn fish_finder(fish: &Fish, query: &str) -> bool {
+    (query.len() > 0 && fish.name.to_lowercase().contains(&query.to_lowercase()))
+        || query.len() == 0
 }
