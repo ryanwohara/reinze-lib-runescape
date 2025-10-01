67ba37fb49ef182e5ec670381b59f748c2347fe9
Ryan William O'Hara <ryan@ryanwohara.com>
2025-10-01 12:53:07 -0600

    fix(ci): cargo update

--- bin/bd-prod.sh
+++ bin/bd-prod.sh
@@ -3,6 +3,7 @@
 
 [[ $(uname -s) == "Darwin" ]] && export extension=.dylib || export extension=.so
 
-cargo build --release && \
+cargo update && \
+  cargo build --release && \
   rm -rf ../rust-reinze/plugins/libreinze_lib_runescape* && \
   cp "target/release/libreinze_lib_runescape${extension}" "../rust-reinze/plugins/libreinze_lib_runescape_$(date "+%Y%m%dT%H%M%S")${extension}"
