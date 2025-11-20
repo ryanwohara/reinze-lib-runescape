383a420f8f292a23f10b4ac9b8178600d806d59c
Ryan William O'Hara <ryan@ryanwohara.com>
2025-11-20 15:31:39 -0700

    fix: Clarify these are positive boosts

--- src/boost.rs
+++ src/boost.rs
@@ -160,11 +160,11 @@ fn get_modifiers(skill: &str) -> Vec<(String, String)> {
             ("Orange spicy stew", "-5/+5"),
         ],
         "Sailing" => vec![
-            ("Kraken ink stout", "1"),
-            ("Perildance bitter", "1"),
-            ("Trawler's trust", "2"),
-            ("Whirlpool surprise", "3"),
-            ("Horizon's lure", "4"),
+            ("Kraken ink stout", "+1"),
+            ("Perildance bitter", "+1"),
+            ("Trawler's trust", "+2"),
+            ("Whirlpool surprise", "+3"),
+            ("Horizon's lure", "+4"),
             ("Yellow spicy stew", "-5/+5"),
         ],
         "Slayer" => vec![
